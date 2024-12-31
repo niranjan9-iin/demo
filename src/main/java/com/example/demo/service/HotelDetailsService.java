4be7913bfb728f1c7e39a425c1f9f4291fa3f29d
Niranjan.Kadu <Niranjan.Kadu@ihg.com>
2024-12-31 16:42:36 +0530

    passing versions to comapre function

--- src/main/java/com/example/demo/service/HotelDetailsService.java
+++ src/main/java/com/example/demo/service/HotelDetailsService.java
@@ -20,13 +20,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class HotelDetailsService {
 	// staging key
-	 private static final String api_key = "EMMyBVyWDU56GpigMGWDlDDf7zucpYjd";
+	private static final String api_key = "EMMyBVyWDU56GpigMGWDlDDf7zucpYjd";
 	// int key
-	//private static final String api_key = "uG3jpwBVdq71uqfT6dvwGFavAjmpQ4Vr";
+	// private static final String api_key = "uG3jpwBVdq71uqfT6dvwGFavAjmpQ4Vr";
 	// staging hostname
-	public static final String hostName ="https://staging-api.ihg.com/hotels/";
+	public static final String hostName = "https://staging-api.ihg.com/hotels/";
 	// int hostname
-	//public static final String hostName = "https://int-api.ihg.com/hotels/";
+	// public static final String hostName = "https://int-api.ihg.com/hotels/";
 	@Autowired
 	private RestTemplate restTemplate;
 	public static ArrayList<String> fieldsets = new ArrayList<>(Arrays.asList("accessibility", "address",
@@ -36,9 +36,11 @@ public class HotelDetailsService {
 			"nonRoomInventory", "parking", "policies", "poolAndFitness", "profile", "publicAreas", "recreations",
 			"renovations", "restaurant", "room", "roomTypes", "safety", "services", "shoppingFacilities", "tax",
 			"technology", "transportation", "weddingDetails", "payments"));
-	//public static ArrayList<String> fieldsets = new ArrayList<>(Arrays.asList("profile"));
+	// public static ArrayList<String> fieldsets = new
+	// ArrayList<>(Arrays.asList("profile"));
 
 	public String getHotelDetails(String hotelCode, String brandCode) {
+		ResponseComparator.result = "";
 		// Generate URLs for v1 and v3
 		String v1 = generateURL("v1", hotelCode, brandCode);
 		String v3 = generateURL("v3", hotelCode, brandCode);
@@ -65,7 +67,7 @@ public class HotelDetailsService {
 			// Compare "hotelContent" and "hotelInfo" nodes
 			if (rootNode2.has("hotelContent") && rootNode1.has("hotelInfo")) {
 				result = ResponseComparator.compareNodes(rootNode2.get("hotelContent").get(0),
-						rootNode1.get("hotelInfo"));
+						rootNode1.get("hotelInfo"), "V1", "V3");
 			} else {
 				System.out.println("No matching nodes (hotelContent and hotelInfo) found for comparison.");
 			}
@@ -77,6 +79,7 @@ public class HotelDetailsService {
 	}
 
 	public String getHotelDetailsPost(HotelDetailRequest hdr) {
+		ResponseComparator.result = "";
 		Object responsev3 = null;
 		Object responsev2 = null;
 		String result = "";
@@ -95,7 +98,7 @@ public class HotelDetailsService {
 			JsonNode rootNode1 = objectMapper.readTree(jsonResponse1);
 			JsonNode rootNode2 = objectMapper.readTree(jsonResponse2);
 			result = ResponseComparator.compareNodes(rootNode2.get("hotelContents").get(0).get("hotelContent"),
-					rootNode1.get(0));
+					rootNode1.get(0), "V2", "V3");
 		} catch (Exception e) {
 			// TODO: handle exception
 			result = "Exception occured while hotelsearch";
--- src/main/java/com/example/demo/service/ResponseComparator.java
+++ src/main/java/com/example/demo/service/ResponseComparator.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class ResponseComparator {
 	public static String result = "";
 
-	public static String compareNodes(JsonNode node1, JsonNode node2) {
+	public static String compareNodes(JsonNode node1, JsonNode node2, String version1, String version2) {
 		System.out.println("Comparing nodes V3 and V1:");
 		// here node1 is v3 api & node2 is v1 api
 		Iterator<String> fieldNames1 = node1.fieldNames();
@@ -17,22 +17,20 @@ public class ResponseComparator {
 			JsonNode value2 = node2.get(fieldName);
 
 			if (value2 == null) {
-				System.out.println("Field " + fieldName + " is missing in V1 API.");
-				result += "\nField " + fieldName + " is missing in V1 API.\n";
+				System.out.println("Field " + fieldName + " is missing in " + version1 + " API.");
+				result += "\nField " + fieldName + " is missing in " + version1 + "  API.\n";
 			} else if (value1.isObject() && value2.isObject()) {
 				System.out.println("Comparing nested object: " + fieldName);
-				compareNodes(value1, value2);
+				compareNodes(value1, value2, version1, version2);
 			} else if (value1.isArray() && value2.isArray()) {
 				System.out.println("Comparing array: " + fieldName);
-				compareArrays(value1, value2, fieldName);
+				compareArrays(value1, value2, fieldName, version1, version2);
 			} else if (!value1.equals(value2)) {
 				if (!compareLocalValues(value1, value2)) {
 					System.out.println("Difference in field " + fieldName + ":");
 					result += "\nDifference in field " + fieldName + ":\n";
-					System.out.println("  API 1 (V3) Value: " + value1);
-					result += "\n  API 1 (V3) Value: " + value1 + "\n";
-					System.out.println("  API 2 (V1) Value: " + value2);
-					result += "\n  API 2 (V1) Value: " + value2 + "\n";
+					result += "\n  API 1 (" + version2 + ") Value: " + value1 + "\n";
+					result += "\n  API 2 (" + version1 + ") Value: " + value2 + "\n";
 				}
 			}
 		}
@@ -42,34 +40,32 @@ public class ResponseComparator {
 		while (fieldNames2.hasNext()) {
 			String fieldName = fieldNames2.next();
 			if (!node1.has(fieldName)) {
-				System.out.println("Field " + fieldName + " is missing in V3 API.");
-				result += "\nField " + fieldName + " is missing in V3 API.\n";
+				System.out.println("Field " + fieldName + " is missing in " + version2 + " API.");
+				result += "\nField " + fieldName + " is missing in " + version2 + " API.\n";
 			}
 		}
 		return result;
 	}
 
-	private static void compareArrays(JsonNode array1, JsonNode array2, String fieldName) {
+	private static void compareArrays(JsonNode array1, JsonNode array2, String fieldName, String version1,
+			String version2) {
 		System.out.println("Comparing arrays for field: " + fieldName);
 		if (array1.size() != array2.size()) {
-			System.out.println("  Array size mismatch: API 1 (v3) size = " + array1.size() + ", API 2 (v1) size = "
-					+ array2.size());
-			result += "\n  Array size mismatch: API 1 (v3) size = " + array1.size() + ", API 2 (v1) size = "
-					+ array2.size() + "\n";
+
+			result += "\n  Array size mismatch: API 1 (" + version2 + ") size = " + array1.size() + ", API 2 ("
+					+ version1 + ") size = " + array2.size() + "\n";
 		} else {
 			for (int i = 0; i < array1.size(); i++) {
 				JsonNode item1 = array1.get(i);
 				JsonNode item2 = array2.get(i);
 				if (!item1.equals(item2)) {
 					if (item1.isObject() && item2.isObject()) {
-						compareNodes(item1, item2);
+						compareNodes(item1, item2, version1, version2);
 					} else {
 						System.out.println("  Difference at index " + i + ":");
 						result += "\n  Difference at index " + i + ":\n";
-						System.out.println("    API 1 (v3) Value: " + item1);
-						result += "\n    API 1 (v3) Value: " + item1 + "\n";
-						System.out.println("    API 2 (v1) Value: " + item2);
-						result += "\n    API 2 (v1) Value: " + item2 + "\n";
+						result += "\n    API 1 (" + version2 + ") Value: " + item1 + "\n";
+						result += "\n    API 2 (" + version1 + ") Value: " + item2 + "\n";
 					}
 				}
 			}
